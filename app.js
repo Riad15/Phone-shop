c204937f697dd3b9fea5ff66e1ddaabcae60238b
asif riad <asifriad15@gmail.com>
2022-02-28 23:48:49 +0600

    search btn functionality created

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+//<---------------search var button function------------------>
 const inputFunction = () => {
-    console.log("clicked");
+    const searchText = document.getElementById('search-box').value;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data[0]));
 }
